c77ea89cce0fc7c35dd311a85291d8e402f77c2d
DevOam <LAKOUAS-MOHAMEDREDA@ofppt-edu.ma>
2023-04-07 16:53:42 +0000

    Spiner

---
+++ Spinner/app/src/main/java/com/example/spinner/ItemData.java
@@ -0,0 +1,13 @@
+package com.example.spinner;
+
+public class ItemData {
+    String name;
+    int image;
+    String Details;
+
+    public ItemData(String name, int image, String details) {
+        this.name = name;
+        this.image = image;
+        Details = details;
+    }
+}
